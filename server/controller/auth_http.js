129cb61ea47de37f57a51bccd1ffea715461f505
beata.cao <beata.cao@vipshop.com>
2019-01-31 19:44:44 +0800

    http digest 认证

--- server/controller/auth_http.js
+++ server/controller/auth_http.js
@@ -1,7 +1,22 @@
 var service_user = require('../service/users')
+var crypto = require('crypto')
+
+var md5UsingCrypto = function(data){
+    return crypto.createHash('md5').update(data, 'utf-8').digest('hex')
+}
+
+var getDigestInfo = function(auth){
+    var digestInfo = auth.replace(/^Digest /, '')
+    var digestObj = {}
+    digestInfo.split(', ').forEach(function(item){
+        var temp = item.split('=')
+        digestObj[temp[0]] = (temp[0] === "qop" || temp[0] === 'nc') ? temp[1] : temp[1].substr(1, temp[1].length -2)
+    })
+    return digestObj
+}
 
 module.exports = {
-    basic: function(req, res, next, realm){
+    basic: function(req, res, next){
         var isAuthorized = false
         var authorization = req.headers.authorization
         if(authorization){
@@ -19,8 +34,37 @@ module.exports = {
         }
         if(!isAuthorized){
             res.statusCode = 401;
-            res.setHeader('WWW-Authenticate', 'Basic realm="'+ realm || req.path +'"');
+            res.setHeader('WWW-Authenticate', 'Basic realm="'+ req.path +'"');
             res.end()
+        }
+        next()
+    },
+    digest: function(req, res, next){
+        var isAuthorized = false
+        var authorization = req.headers.authorization
+        if(authorization){
+            var digestObj = getDigestInfo(authorization)
+            var user = service_user.getUsers(digestObj.username)
+            var digestAuthObj = {}
+            var realm = req.path
+
+            digestAuthObj.ha1 = md5UsingCrypto(digestObj.username + ':' + digestObj.realm + ':' + user.password);
+            digestAuthObj.ha2 = md5UsingCrypto(req.method + ':' + digestObj.uri);
+        
+            var resp = md5UsingCrypto([digestAuthObj.ha1, digestObj.nonce, digestObj.nc, digestObj.cnonce, digestObj.qop, digestAuthObj.ha2].join(':'));
+                    
+            digestAuthObj.response = resp;
+
+            if (digestObj.response === digestAuthObj.response) {
+                isAuthorized = true
+            }
+        }
+        if(!isAuthorized){
+            var nonce = md5UsingCrypto(Math.random() + req.path)
+            var opaque = md5UsingCrypto(req.path)
+            res.statusCode = 401
+            res.setHeader('WWW-Authenticate', 'Digest realm="' + req.path + '", qop="auth", nonce="' + nonce + '", opaque="' + opaque + '"')
+            res.end();
         }
         next()
     }
--- server/router.js
+++ server/router.js
@@ -4,11 +4,15 @@ var auth_http = require('./controller/auth_http')
 var router = express.Router()
 
 router.get('/docs', auth_http.basic, function(req, res){
-    res.render('docs')
+    res.end('docs')
+})
+
+router.get('/articles', auth_http.digest, function(req, res){
+    res.end('articles')
 })
 
 router.get('/logout', function(req, res){
-    res.send('logout')
+    res.end('logout')
 })
 
 module.exports = router
